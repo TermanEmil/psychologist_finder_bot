97ce5dd3f36907623a4c0adbaf8171d7c6d33b41
TermanEmil <terman.emil@gmail.com>
2022-03-22 21:42:14 +0100

    Move the imports to their required lambdas

--- handle_bot_message/app.py
+++ handle_bot_message/app.py
@@ -1,14 +1,12 @@
 import json
 from dataclasses import asdict
 
-import telegram
-
-from SubmittedForm import get_all_submitted_forms
-from message_handlers.main import handle_message
-from telegram_bot import get_bot
-
 
 def lambda_handler(event, context):
+    import telegram
+    from telegram_bot import get_bot
+    from message_handlers.main import handle_message
+
     update = telegram.Update.de_json(json.loads(event['body']), get_bot())
     handle_message(update)
 
@@ -16,8 +14,9 @@ def lambda_handler(event, context):
 
 
 def lambda_handler_get_submitted_forms(event, context):
-    forms = [asdict(form) for form in get_all_submitted_forms()]
+    from SubmittedForm import get_all_submitted_forms
 
+    forms = [asdict(form) for form in get_all_submitted_forms()]
     return {
         'statusCode': 200,
         'body': json.dumps({'forms': forms}, ensure_ascii=False).encode('utf8')
