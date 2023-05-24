bec88f0ce58716f5f627bfeafc923b02d6435ed4
TermanEmil <terman.emil@gmail.com>
2023-05-25 01:48:17 +0200

    Increase timeout

--- handle_bot_message/app.py
+++ handle_bot_message/app.py
@@ -27,18 +27,18 @@ def get_application(context) -> Application:
     return _application
 
 def lambda_handler(event, context):
-    # from message_handlers.main import handle_message
+    from message_handlers.main import handle_message
 
     print('Starting handling')
-    # application = get_application(context)
-    # application.add_handler(MessageHandler(filters.ALL, handle_message))
-    #
-    # try:
-    #     update = Update.de_json(json.loads(event['body']), application.bot)
-    #     asyncio.get_event_loop().run_until_complete(application.process_update(update))
-    # except Exception as e:
-    #     print(e)
-    #     return {"statusCode": 500}
+    application = get_application(context)
+    application.add_handler(MessageHandler(filters.ALL, handle_message))
+
+    try:
+        update = Update.de_json(json.loads(event['body']), application.bot)
+        asyncio.get_event_loop().run_until_complete(application.process_update(update))
+    except Exception as e:
+        print(e)
+        return {"statusCode": 500}
 
     return {"statusCode": 204}
 
