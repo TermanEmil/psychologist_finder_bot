34b9122fffba6c1804f8cfb0ccb0583d9f0853e2
TermanEmil <terman.emil@gmail.com>
2023-05-25 01:35:11 +0200

    It's timeing out somewhere

--- handle_bot_message/app.py
+++ handle_bot_message/app.py
@@ -30,15 +30,15 @@ def lambda_handler(event, context):
     from message_handlers.main import handle_message
 
     print('Starting handling')
-    application = get_application(context)
-    application.add_handler(MessageHandler(filters.ALL, handle_message))
-
-    try:
-        update = Update.de_json(json.loads(event['body']), application.bot)
-        asyncio.get_event_loop().run_until_complete(application.process_update(update))
-    except Exception as e:
-        print(e)
-        return {"statusCode": 500}
+    # application = get_application(context)
+    # application.add_handler(MessageHandler(filters.ALL, handle_message))
+    #
+    # try:
+    #     update = Update.de_json(json.loads(event['body']), application.bot)
+    #     asyncio.get_event_loop().run_until_complete(application.process_update(update))
+    # except Exception as e:
+    #     print(e)
+    #     return {"statusCode": 500}
 
     return {"statusCode": 204}
 
