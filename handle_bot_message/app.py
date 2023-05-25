905cd999be20a0759111ec8b320e420840a095f5
TermanEmil <terman.emil@gmail.com>
2023-05-25 23:32:35 +0200

    Replace Application with Bot

--- handle_bot_message/app.py
+++ handle_bot_message/app.py
@@ -3,39 +3,25 @@ import json
 import os
 from dataclasses import asdict
 
-from telegram import Update
-from telegram.ext import Application, MessageHandler, filters
+from telegram import Update, Bot
 
 
-_application = None
+def get_bot_token(context) -> str:
+    if ':Prod' in context.invoked_function_arn:
+        environment = ''
+    else:
+        environment = 'DEV'
 
-def get_application(context) -> Application:
-    global _application
+    return os.environ.get(f"{environment}_TELEGRAM_BOT_TOKEN")
 
-    if _application is None:
-        print('Acquiring Telegram Bot connection')
-
-        if ':Prod' in context.invoked_function_arn:
-            environment = ''
-        else:
-            environment = 'DEV'
-
-        bot_token = os.environ.get(f"{environment}_TELEGRAM_BOT_TOKEN")
-        _application = Application.builder().token(bot_token).build()
-        print('Finished acquiring Telegram Bot connection')
-
-    return _application
 
 def lambda_handler(event, context):
     from message_handlers.main import handle_message
 
-    print('Starting handling')
-    application = get_application(context)
-    application.add_handler(MessageHandler(filters.ALL, handle_message))
-
     try:
-        update = Update.de_json(json.loads(event['body']), application.bot)
-        asyncio.get_event_loop().run_until_complete(application.process_update(update))
+        with Bot(token=get_bot_token(context)) as bot:
+            update = Update.de_json(json.loads(event['body']), bot)
+            asyncio.get_event_loop().run_until_complete(handle_message(update, None))
     except Exception as e:
         print(e)
         return {"statusCode": 500}
